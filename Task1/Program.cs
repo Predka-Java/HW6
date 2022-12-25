021580ab71426d833e809d1ceacd9387752d767c
Predka <swaggajugga69@gmail.com>
2022-12-26 01:25:00 +0700

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+int ReceiveCount(string text, int size)
+{
+    Console.WriteLine(text);
+    string str = string.Empty;
+    int count = 0;
+    for (int i = 0; i < size; i++)
+    {
+        int num = int.Parse(Console.ReadLine());
+        if(num > 0)
+        {
+            count++;
+            Console.WriteLine($" Количество положительных чисел: {count}");
+        } 
+        else
+        {
+             Console.WriteLine($" Количество положительных чисел: {count}");
+        }
+    }
+    return count;
+}
+int ReceiveSize(string text) {
+    Console.Write(text);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+int size = ReceiveSize("Сколько чисел обработать?");
+
+int res = ReceiveCount("Введите число ", size);
+
+Console.WriteLine("Помучился =(");
